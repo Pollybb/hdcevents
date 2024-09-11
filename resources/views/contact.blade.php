dba0433aebbeb8e0b2d440eff276abc115dd134a
Pollyana <131013385+Pollybb@users.noreply.github.com>
2024-09-10 22:25:15 -0300

    Initial commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+
+@extends('layouts.main')
+
+@section('title', 'contato')
+
+@section('content')
+
+        <h1>Contatos</h1>
+        <a href="/"> Voltar a tela d home</a>
+
+        
+@endsection    
